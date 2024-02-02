97250132d1cc25aa37af3177fb1fb644aa283bf2
Rainer Stropek <rainer@software-architects.at>
2024-02-02 07:16:24 +0100

    Initial

---
+++ starter/TrainSwitching.Logic/SwitchingOperationParser.cs
@@ -0,0 +1,15 @@
+namespace TrainSwitching.Logic;
+
+public static class SwitchingOperationParser
+{
+    /// <summary>
+    /// Parses a line of input into a <see cref="SwitchingOperation"/>.
+    /// </summary>
+    /// <param name="inputLine">Line to parse. See readme.md for details</param>
+    /// <returns>The parsed switching operation</returns>
+    public static SwitchingOperation Parse(string inputLine)
+    {
+        // TODO: Implement this method
+        throw new NotImplementedException();
+    }
+}
